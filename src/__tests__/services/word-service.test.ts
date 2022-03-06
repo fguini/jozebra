26a378c7b10ff216d0114e4e1ee1938d9f192fd8
Facundo Guini <fguini@gmail.com>
2022-03-06 18:02:23 +0100

    word service unit tests

---
+++ src/__tests__/services/word-service.test.ts
@@ -0,0 +1,33 @@
+import { Cache } from '../../utils/cache-utils';
+import { getTodayWord, getWords } from '../../services/word-service';
+
+describe('word service', () => {
+    describe('getWords()', () => {
+        it('Should return a list of words', () => {
+            const words = getWords();
+
+            expect(words.length).toBeGreaterThan(1);
+            expect(typeof words[0]).toBe('string');
+        })
+    });
+
+    describe('getTodayWord()', () => {
+        let cachedWord: string | null = null;
+        afterAll(() => {
+           Cache.clear();
+        });
+
+        it('Should get today word', async () => {
+            const word = await getTodayWord();
+            cachedWord = word;
+
+            expect(typeof word).toBe('string');
+        });
+
+        it('Should same today word on the second call of the day', async () => {
+            const word = await getTodayWord();
+
+            expect(word).toBe(cachedWord);
+        });
+    });
+});
