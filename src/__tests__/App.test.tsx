01ec23c7392571be87c079bd016ee231fcd075d0
Facundo Guini <fguini@gmail.com>
2022-03-19 23:02:28 +0100

    App integration tests

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from '../App';
 
-describe.skip('<App />', () => {
-    it('Should render home screen when root path', () => {
+describe('<App />', () => {
+    it('Should render home screen when root path', async () => {
+        const spyLetterBox = jest.spyOn(require('../components/jozebra/Attempts/LetterBox'), 'LetterBox');
         render(<App />);
+
+        screen.getByRole('heading', { name: /app.title/ });
+        screen.getByText('Loading...');
+
+        await waitFor(() => {
+            expect(spyLetterBox).toBeCalledTimes(5 * 6 * 2);
+        });
+        spyLetterBox.mockRestore();
     });
 });
